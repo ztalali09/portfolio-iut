8c3de9b1e2ac2864e6953e2e3304901ca2d8e1cf
ztalali09 <talalizakariapro@gmail.com>
2025-06-12 14:06:43 +0200

    Refactor animation properties in Header component: rename slide to slideUp and adjust initial, enter, and exit values for improved animation behavior.

--- src/components/Header/animation.js
+++ src/components/Header/animation.js
@@ -1,13 +1,13 @@
 export const menuSlide = {
-    initial: {x: "calc(100% + 100px)"},
-    enter: {x: "0", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}},
-    exit: {x: "calc(100% + 100px)", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}}
+    initial: { x: "100%" },
+    enter: { x: "0", transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] } },
+    exit: { x: "100%", transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] } }
 }
 
-export const slide = {
-    initial: {x: 80},
-    enter: i => ({x: 0, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}}),
-    exit: i => ({x: 80, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}})
+export const slideUp = {
+    initial: { y: 80 },
+    enter: { y: 0, transition: { duration: 0.6, ease: [0.76, 0, 0.24, 1] } },
+    exit: { y: 80, transition: { duration: 0.6, ease: [0.76, 0, 0.24, 1] } }
 }
 
 export const scale = {
